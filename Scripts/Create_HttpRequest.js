6dcedd2ed6f418c56328c0882c3451fc3145f081
AhmedMustafa <ahmed.mustafa@d360.com>
2024-04-01 16:25:54 +0200

    Remove unused local variables

--- Scripts/Create_HttpRequest.js
+++ Scripts/Create_HttpRequest.js
@@ -3,7 +3,6 @@ import http from 'k6/http';
 
 export default function () {
     let URL = 'https://test-api.k6.io';
-    let pageTitle = 'Collection of HTTP and WebSocket APIs for experimentation with k6';
     const res = http.get(URL);
     console.log(res.status)
 }
